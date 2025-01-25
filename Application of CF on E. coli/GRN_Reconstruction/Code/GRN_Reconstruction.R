fe355fc8003944a3abe375ba1c0554a83a2a55d6
Nilesh-Subramanian <60778368+Nilesh-Subramanian@users.noreply.github.com>
2025-01-25 08:46:20 +0530

    Add files via upload

---
+++ Application of CF on E. coli/GRN_Reconstruction/Code/GRN_Reconstruction.R
@@ -0,0 +1,22 @@
+dag_wl <- read_csv("DAG_WL.csv")
+GE <- read_csv("GE.csv")
+
+library(bnlearn)
+bn_TR <- hc(total_ecoli_bin, whitelist = dag_wl,max.iter = 100, debug = T, score = "bde", maxp = 5)
+
+setwd("/data/users/cs20d300/WORK_MAIN/Integrated_ntwk/Ecoli/TRIMER/GRN/BDE/results/TRIMER/")
+saveRDS(bn_TR, "bn_TR.RDS")
+
+
+bn_params_TR  <- bn.fit(bn_TR, total_ecoli_bin, method = "bayes", debug = T)
+setwd("/data/users/cs20d300/WORK_MAIN/Integrated_ntwk/Ecoli/TRIMER/GRN/BDE/results/TRIMER/")
+saveRDS(bn_params_TR, "bn_params_TR.RDS")
+
+
+
+
+
+
+
+
+
