eeceac4cd1b2bbb251a943e40ebec546a3a123b1
Nilesh-Subramanian <60778368+Nilesh-Subramanian@users.noreply.github.com>
2025-01-25 10:06:45 +0530

    Update Bin_Pred_analysis.R

--- Application of CF on E. coli/Results/Bin_Pred_analysis.R
+++ Application of CF on E. coli/Results/Bin_Pred_analysis.R
@@ -1,1 +1,6 @@
+library(caret)
 
+Bin_Data_Ecoli <- data.frame(Act_vs_Pred_Ecoli$Gene,act_bin, pred_bin_cfs, pred_bin_cfmtr, pred_bin_tri)
+colnames(Bin_Data_Ecoli) <- c("Genes","Binarized_Actual","Binarized_CFS_predictions","Binarized_CFMTR_predictions","Binarized_TRIMER_predictions")
+
+write.csv(Bin_Data_Ecoli,"Binarized_act_pred_ecoli.csv", row.names = F)
