354a69adae3fea954fd36112d9b52bfee6a4d073
Nilesh-Subramanian <60778368+Nilesh-Subramanian@users.noreply.github.com>
2026-03-31 16:49:52 +0530

    Update M_2a.m

--- Application of CF on E. coli/CausalFlux_Runs/M_2a.m
+++ Application of CF on E. coli/CausalFlux_Runs/M_2a.m
@@ -1,4 +1,3 @@
-% matlab function - step 2a  - FVA with 0 objective function 
 curr_wd = 'D:\work\Integrated_network_model\Git_hub_codes\Ecoli\CF';
 cd(curr_wd)
 
@@ -6,29 +5,21 @@ Updated_FVA_round_i = "Updated_FVA_round_P1_i.xlsx";
 Updated_FVA_round_i = readtable(Updated_FVA_round_i, "VariableNamingRule","preserve");
 
 % some pre-requisites 
-% initCobraToolbox(false);
-% changeCobraSolver('gurobi', 'all');
-load('D:/work/Integrated_network_model/Toy_model/auto_new_model_current_approach_27_03_24/Causal_Surgery/environment')
-restoreEnvironment(environment);
+ initCobraToolbox(false);
+ changeCobraSolver('gurobi', 'all');
 
-
-% fileName = 'Ecoli_model_aerobic_sink_no_media.mat'; 
-% fileName = 'TR_based_modified_ecoli.mat';
-
-%fileName = 'Ecoli_sink_no_media_aerobic_iML1515.mat';
 fileName = 'NEW_iML1515_modified_sink.mat';
 TM_0 = readCbModel(fileName);
     
-
 cd(curr_wd)
 exch_rxns_dt = readtable("Exch_iml1515.csv");
 
 TM_0.lb(exch_rxns_dt.Var1) = 0;
 
-
 cd(curr_wd)
 essen_exch_rxns_dt = readtable("essential_exch_rxns_ids_iml1515.csv");
 TM_0.lb(essen_exch_rxns_dt.Var1)= -0.001;
+
 
 cd(curr_wd)
 LBmediaconstraints = readtable("LB_media_constraints_iML1515.csv");
@@ -42,17 +33,10 @@ TM_0.ub(LBmediaconstraints.media_list)=-LBmediaconstraints.media_list_ub;
 TM_0.lb(181) = -readvars("Exch_G.csv");   %iML1515
 TM_0.lb(1982) = -readvars("Exch_O.csv");
 
-%  TM_0.ub(2383:2448) = 1000;
 
 TM_0.ub(2713:2731) = 10; % iML1515
 
 x_model_round_i = TM_0;
-
-% x_model_round_i.c(926)=1; % IAF1260 - BIOMASS
-% x_model_round_i.c(1005)=1; % TR EcoMac - Biomass
-
-% x_model_round_i.c(2267)=1;  % for our IAF1260 - Indole
-% x_model_round_i.c(2272)=1;  % for TR Ecoli - Indole
 
 x_model_round_i.c(2669)=1; % for iML1515
 
