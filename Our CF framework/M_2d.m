41f6d3e74c7884d01531a2f4157dae3c5f631738
Nilesh-Subramanian <60778368+Nilesh-Subramanian@users.noreply.github.com>
2026-04-02 21:07:41 +0530

    Update M_2d.m

--- Our CF framework/M_2d.m
+++ Our CF framework/M_2d.m
@@ -2,27 +2,25 @@ curr_wd = 'D:\work\Integrated_network_model\Git_hub_codes\Ecoli\CF';
 cd(curr_wd)
 initCobraToolbox(false);
 changeCobraSolver('gurobi', 'all');
-
 cp_r1 = "CP_round_P1_i.xlsx";
 cp_r1 = readtable(cp_r1);
-
 gene_cp_r1.value = cp_r1.Probability ; gene_cp_r1.gene = cp_r1.Bigg_symb;
 
-fileName = 'NEW_iML1515_modified_sink.mat';
+fileName = 'NEW_iML1515_modified_sink.mat';    %%% load the metabolic model 
 TM_0 = readCbModel(fileName);
 
 cd(curr_wd)
-exch_rxns_dt = readtable("Exch_iml1515.csv");
+exch_rxns_dt = readtable("Exch_iml1515.csv");   %%% a dataframe with all exchange reactions in the metabolic model and its index
 
 TM_0.lb(exch_rxns_dt.Var1) = 0;
 
+
 cd(curr_wd)
-essen_exch_rxns_dt = readtable("essential_exch_rxns_ids_iml1515.csv");
+essen_exch_rxns_dt = readtable("essential_exch_rxns_ids_iml1515.csv");  %%% list of index of the essential exchange reactions in the metabolic model
 TM_0.lb(essen_exch_rxns_dt.Var1)= -0.001;
 
-
 cd(curr_wd)
-LBmediaconstraints = readtable("LB_media_constraints_iML1515.csv");
+LBmediaconstraints = readtable("LB_media_constraints_iML1515.csv");  %%% a dataframe with index of the exchange reactions for media condition and the upper bounds for those reactions
 % LBmediaconstraints = readtable("M9_serine_galacturonate.csv");
 % LBmediaconstraints = readtable("TSBmed.csv");
 
@@ -30,10 +28,10 @@ LBmediaconstraints = readtable("LB_media_constraints_iML1515.csv");
 
 
 cd(curr_wd)
-TM_0.lb(181) = -readvars("Exch_G.csv");   %iML1515 glucose exchange reaction 
-TM_0.lb(1982) = -readvars("Exch_O.csv");  %iML1515 oxygen exchange reaction 
+TM_0.lb(181) = -readvars("Exch_G.csv");   %%% glucose exchange reaction in the metabolic model
+TM_0.lb(1982) = -readvars("Exch_O.csv");   %%% oxygen exchange reaction in the metabolic model 
 
-TM_0.ub(2713:2731) = 10; % iML1515 extra sink reactions added based on metabolic feedback information 
+TM_0.ub(2713:2731) = 10; %%% indices specifying start and end of added sink reactions 
 
 gpr_eval_r1 = mapExpressionToReactions(TM_0,gene_cp_r1);
 TF = isnan(gpr_eval_r1);
